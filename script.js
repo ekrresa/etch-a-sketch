9aa3ffee310e44bed1b4a82e6376e60f473f53a6
ochuko56 <kelvinekrresa@gmail.com>
2018-05-21 11:48:34 +0100

    initial commit: setup container + div grid

---
+++ script.js
@@ -0,0 +1,8 @@
+var squares = 500/10;
+
+for (var i = 0; i < 100; i++) {
+        var div = document.createElement("div");
+        document.querySelector(".container").appendChild(div);
+    // var jump = document.createElement("br");
+    // document.querySelector(".container").appendChild(jump);
+}
