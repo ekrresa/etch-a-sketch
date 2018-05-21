af56a70caf4f8a25ed7f510871295db32f336e06
ochuko56 <kelvinekrresa@gmail.com>
2018-05-21 18:29:12 +0100

    show random colors when hovering over squares

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createGrid() {
         div.className = 'square';
         div.style.height = divSize + 'px';
         div.style.width = divSize + 'px';
-    	div.addEventListener("mouseover", black);
+    	div.addEventListener("mouseover", generateColor);
 
 
         document.querySelector(".container").appendChild(div);
@@ -25,11 +25,6 @@ function generateColor() {
 	for (var i = 0; i < 6; i++ ) {
 	    color += letters[Math.round(Math.random() * 15)];
 	}
-	return color;
-}
-
-function black () {
-	this.style.cursor = 'pointer';
-	this.style.background = 'black';
+	this.style.background = color;
 }
 
